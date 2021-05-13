e47d9b18206a61857b4c6471002a73caccb55811
Sagar Adval <sagaradval@gmail.com>
2021-05-13 18:01:51 +0530

    Added bundling in browser

--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,52 @@
-import { useEffect, useState, useRef } from 'react';
-import ReactDOM from 'react-dom';
 import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
-const App = () =>{
+const App = () => {
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
-    const ref = useRef<any>();
-    const [input, setInput] = useState('');
-    const [code, setCode] = useState('');
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+  useEffect(() => {
+    startService();
+  }, []);
 
-    const onClickHandler = async () => {
-        if(!ref.current) {
-            return;
-        }
-
-        const res = await ref.current.transform(input, {
-            loader: 'jsx',
-            target: 'es2015'
-        })
-
-        setCode(res.code);
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
     }
 
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: '/esbuild.wasm'
-        })
-    }
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
 
-    useEffect(() => {
-        startService();
-    }, [])
+    // console.log(result);
 
-    return (
-        <div>
-            <textarea value = {input} onChange = {e => setInput(e.target.value)}></textarea>
-            <div>
-                <button onClick = {onClickHandler}>Submit</button>
-            </div>
-            <pre>{code}</pre>
-        </div>
-    )
-}
+    setCode(result.outputFiles[0].text);
+  };
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
