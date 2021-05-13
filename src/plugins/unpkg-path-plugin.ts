4c479cdf16dc6d91c2568d9d208bd01887c1eeb6
Sagar Adval <sagaradval@gmail.com>
2021-05-13 18:17:17 +0530

    Added fetch of module from unpkg

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,26 +1,36 @@
-import * as esbuild from 'esbuild-wasm';
+import * as esbuild from "esbuild-wasm";
+import axios from "axios";
 
 export const unpkgPathPlugin = () => {
   return {
-    name: 'unpkg-path-plugin',
+    name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-        return { path: args.path, namespace: 'a' };
+        console.log("onResole", args);
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
+        }
+        return { path: `https://unpkg.com/${args.path}`, namespace: "a" };
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
+        console.log("onLoad", args);
 
-        if (args.path === 'index.js') {
+        if (args.path === "index.js") {
           return {
-            loader: 'jsx',
+            loader: "jsx",
             contents: `
               import message from 'tiny-test-pkg';
               console.log(message);
             `,
           };
         }
+
+        const res = await axios(args.path);
+        return {
+          loader: "jsx",
+          contents: res.data,
+        };
       });
     },
   };
