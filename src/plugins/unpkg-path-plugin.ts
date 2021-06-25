5bc8d1732e915c1b03b57f3424c251ed08ab3aa3
Sagar Adval <sagaradval@gmail.com>
2021-06-25 15:29:06 +0530

    resolved nested path issues with resolveDir

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -12,7 +12,7 @@ export const unpkgPathPlugin = () => {
         }
 
         if(args.path.includes('./') || args.path.includes('../')) {
-          return { path: new URL(args.path, args.importer + '/').href, namespace: "a" };
+          return { path: new URL(args.path, 'https://unpkg.com'+ args.resolveDir+ '/').href, namespace: "a" };
         }
 
         else return { path: `https://unpkg.com/${args.path}`, namespace: "a" };
@@ -25,16 +25,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              const message = require('medium-test-pkg');
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           };
         } else {
-          const { data } = await axios.get(args.path);
-          // console.log(data);
+          const { data, request } = await axios.get(args.path);
+          console.log(request);
           return {
             loader: 'jsx',
-            contents: data
+            contents: data,
+            resolveDir: new URL('./', request.responseURL).pathname
           }
         }
 
